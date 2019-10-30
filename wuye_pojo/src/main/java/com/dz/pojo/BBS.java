29ce03de6e69a728fd838f944496a6f1b6007216
zhai123 <2290252138@qq.com>
2019-10-30 18:37:48 +0800

    10.30 18:37 bbs

--- wuye_pojo/src/main/java/com/dz/pojo/BBS.java
+++ wuye_pojo/src/main/java/com/dz/pojo/BBS.java
@@ -6,6 +6,15 @@ public class BBS {
     private String content;
     private String createtime;
     private int userid;
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public int getId() {
         return id;
