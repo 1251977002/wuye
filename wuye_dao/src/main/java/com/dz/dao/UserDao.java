d938a2a1a0dcf288a1a4846dd7b08551c9764cd4
tingtingya <1787697908@qq.com>
2019-11-05 10:09:47 +0800

    sunting

--- wuye_dao/src/main/java/com/dz/dao/UserDao.java
+++ wuye_dao/src/main/java/com/dz/dao/UserDao.java
@@ -97,19 +97,9 @@ public interface UserDao {
     User findUserByUserid(Integer userid);
 
 
-
     /*模糊查询通过username*/
     @Select("SELECT * FROM t_user WHERE username LIKE '%#{username}%'")
     void findUserByusername(String username);
 
-/*    *//*通过owemoney查找用户*//*
-    @Select("select * from t_user where ownmoney >0")
-    @Results({
-            @Result(id = true, column = "id", property = "id"),
-            @Result(property = "propertList",column = "id",many = @Many(select = "com.dz.dao.OweDao.findByuserid"))
-    })
-    List<User> findPageByOweMoney(int pageNum);*/
 
-    @SelectProvider(type=com.dz.dao.provider.GetUserSql.class,method = "getPropertSQL")
-    List<User> findPageByOweMoney(Map<String ,Object> map);
 }
